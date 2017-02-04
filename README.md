e6401044f4b5f8457282b1619b138de8e650e9f8
Eugene Khashin <adastreamer@gmail.com>
2017-02-05 01:14:46 +0300

    swanager sync action added

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ run:
 AZURE_STORAGE_ACCOUNT=account AZURE_STORAGE_ACCESS_KEY=key python -m src.azure_sync ./local/path sharename[/remote/path]
 ```
 
+
 run docker container:
 ```
 docker run -i -t -e AZURE_STORAGE_ACCOUNT=account -e AZURE_STORAGE_ACCESS_KEY=key -v /local/dir:/container/dir dokkur/azure-fileshare-sync /container/dir fileshare[/remote/dir]
+```
+
+
+run swanageer sync:
+```
+AZURE_STORAGE_ACCOUNT=account AZURE_STORAGE_ACCESS_KEY=key python -m src.swanager_sync /path/to/swanager.json sharename[/remote/path]
 ```
--- src/swanager_sync.py
+++ src/swanager_sync.py
@@ -1,2 +1,30 @@
+import os
+import sys
+import json
 import azure_sync
+
+if __name__ == '__main__':
+    config_file = sys.argv[1]
+    remote = sys.argv[2]
+
+    try:
+        remote_parts = remote.split(os.sep)
+        fileshare = remote_parts[0]
+        base_dst = os.sep.join(remote_parts[1:])
+    except IndexError:
+        fileshare = remote
+        base_dst = ''
+
+    with open(config_file) as json_data:
+        config = json.load(json_data)
+
+    project_dir = os.path.dirname(os.path.realpath(config_file))
+
+    for volume in config['volumes']:
+        volume_src = volume['src']
+        volume_dest = volume['dest'][1:]
+        src = os.path.join(project_dir, volume_src)
+        dst = os.path.join(base_dst, volume_dest)
+
+        azure_sync.go(src, fileshare, dst)
 
